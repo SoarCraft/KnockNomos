51c131ea486b6def106ba08570e403ca6a48f3d1
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-20 12:09:23 +0200

    chore: Update project dependencies and remove unused code

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Main } from './main';
-import './index.css'
+import { Button, FluentProvider, webLightTheme } from "@fluentui/react-components";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-
-const container = document.querySelector('#root');
+const container = document.querySelector("#root")!;
 const root = createRoot(container);
 
-root.render(<Main />);
+root.render(
+  <FluentProvider theme={webLightTheme}>
+    <div>
+      <h1>Hello, world!</h1>
+
+      <Button>Click me</Button>
+    </div>
+  </FluentProvider>
+);
