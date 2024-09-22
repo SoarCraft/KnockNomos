37cb07c99ce9a54e74ea16450de26c29e979de81
Harry.G. <42864587+HarryG55@users.noreply.github.com>
2024-09-22 15:26:51 +0800

    fix:修正AudioContext溢出问题

--- src/Components/BaseKnock.tsx
+++ src/Components/BaseKnock.tsx
@@ -1,35 +1,47 @@
-import { symlink } from 'fs';
 import React, { useState, useEffect } from 'react';
 
 export function BaseKnock() {
     // 循环定时器Id
-    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+    const [intervalId, setIntervalId] = useState<number | null>(null);
     // 是否开启运行按钮
-    const [isRunning, setIsRunning] = useState(false);
+    const [isRunning, setIsRunning] = useState<boolean>(false);
     // bpm设定
     const [bpm, setBpm] = useState<number | null>(60);
     // 拍数设定
     const [beat, setBeat] = useState<number | null>(0);
-
+    // 切分拍标记设定
     const [syncopation, setSyncopation] = useState<string | null>('');
 
+    // 当前拍计数器
+    let currentBeat:number = 0;
+
+    const audioContextRef = React.useRef(new (window.AudioContext)());
+    if(audioContextRef.current === null){
+        audioContextRef.current = new (window.AudioContext)();
+    }
 
     const toggleTimer = () => {
         setIsRunning(!isRunning);
     };
 
-    // bpm转换为ms
+    /**
+     * bpm转换为ms
+     * @param bpm 
+     * @returns 转换后对应的ms
+     * 
+     */
     const bpmTranferMs = (bpm: number) => {
         return (1000 * 60) / bpm;
     };
 
-    // 当前拍计数器
-    let currentBeat:number = 0;
-
-    // 发声
-    // beatNum: 0: 强拍, 其他: 弱拍
+    /**
+     * 发出一个声音
+     * beatNum: 0: 强拍, 其他: 弱拍
+     * @param beatNum 拍数
+     * @author HarryG
+     */
     const beep = (beatNum: number) => {
-        const audioContext = new (window.AudioContext)();
+        const audioContext = audioContextRef.current;
         const oscillator = audioContext.createOscillator();
         const gainNode = audioContext.createGain();
         oscillator.connect(gainNode);
@@ -37,8 +49,11 @@ export function BaseKnock() {
 
         if (beatNum === 0) {
             oscillator.frequency.value = 880;
-        } else {
+        } else if(beatNum === 99) {
+            // 切分拍专用
             oscillator.frequency.value = 220;
+        } else {
+            oscillator.frequency.value = 440;
         }
 
         oscillator.type = 'sine';
@@ -47,29 +62,40 @@ export function BaseKnock() {
         oscillator.start();
         setTimeout(() => {
             oscillator.stop();
-        }, 100);
+        }, 50);
     };
 
-    const tick = (ms:number) => {
-        beep(currentBeat);
-        
-        // 切分
+    /**
+     * 切分拍处理
+     * @param ms 拍数延时
+     * @author HarryG
+     */
+    const syncopationBeep = (ms:number) => {
         if(syncopation === '1/4') {
-            setTimeout(() => {beep(currentBeat)}, ms*1/4);
+            setTimeout(() => {beep(99)}, ms*1/4);
         }
         if(syncopation === '3/4') {
-            setTimeout(() => {beep(currentBeat)}, ms*3/4);
+            setTimeout(() => {beep(99)}, ms*3/4);
         }
-
-        currentBeat = (currentBeat + 1) % beat;
-        console.log('tick');
-    };
-
-    const tickOnce = (ms:number, currentBeat:number) => {
-        beep(currentBeat);
-        currentBeat = (currentBeat + 1) % beat;
-        console.log('tick');
+        if(syncopation === '16分') {
+            setTimeout(() => {beep(99)}, ms*1/4);
+            setTimeout(() => {beep(99)}, ms*2/4);
+            setTimeout(() => {beep(99)}, ms*3/4);
+        }
     }
+
+    /**
+     * 定时器处理函数
+     * @param ms 拍数延时
+     */
+    const tick = (ms:number) => {
+        // 正拍处理
+        beep(currentBeat);
+        // 切分后使用弱拍处理，只有一个强拍用于指示起始拍
+        currentBeat = (currentBeat + 1) % beat;
+        // 切分拍处理
+        syncopationBeep(ms);
+    };
 
     useEffect(() => {
         if (isRunning) {
@@ -110,6 +136,7 @@ export function BaseKnock() {
                 <option value="none">无</option>
                 <option value="1/4">前1/4</option>
                 <option value="3/4">后1/4</option>
+                <option value="16分">16分</option>
             </select>
 
 
